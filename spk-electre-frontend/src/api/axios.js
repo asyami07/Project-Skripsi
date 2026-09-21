20804875f5e77224b029b6e4230a0ebce14c383c
Andika <andikafrasetya422@gmail.com>
2026-09-21 10:10:57 +0700

    Configure frontend api environment

--- spk-electre-frontend/src/api/axios.js
+++ spk-electre-frontend/src/api/axios.js
@@ -1,8 +1,31 @@
+// import axios from "axios";
+
+// // Base URL backend API
+// const api = axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
+
+// // otomatis kirim token JWT
+// api.interceptors.request.use((config) => {
+//   // ambil token dari localStorage
+//   const token = localStorage.getItem("token");
+
+//   // jika token ada
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default api;
+
+// DEPLOYMENT
 import axios from "axios";
 
 // Base URL backend API
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // otomatis kirim token JWT
